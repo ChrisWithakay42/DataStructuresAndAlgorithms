33fd70cebfe0f52611a0768332e1be5593d3e2d0
ChrisWithakay42 <codewithakay@gmail.com>
2023-07-19 15:52:23 +0100

    append and pop methods for DLL

--- DoublyLinkedLists/doubly_linked_list.py
+++ DoublyLinkedLists/doubly_linked_list.py
@@ -1,15 +1,40 @@
+from typing import Any
+
+
 class Node:
-    def __init__(self, value):
+    def __init__(self, value: Any) -> None:
         self.value = value
         self.next = None
         self.prev = None
 
 
 class DoublyLinkedList:
-    def __init__(self, value):
+    def __init__(self, value: Node) -> None:
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
         self.length = 1
 
+    def append(self, value: Node) -> bool:
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+        self.length += 1
+        return True
 
+    def pop(self) -> Node | None:
+        if self.length == 0:
+            return None
+        temp = self.tail
+        self.tail = self.tail.prev
+        self.tail.next = None
+        temp.prev = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
